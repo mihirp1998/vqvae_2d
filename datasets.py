2847f8575d8163ba3bb3f2cd25d686c48dc68513
Mihir Prabhudesai <mihirp1998.mp@gmail.com>
2020-01-18 22:35:32 -0500

    fixed bugs and added clevr code

--- datasets.py
+++ datasets.py
@@ -2,6 +2,11 @@ import os
 import csv
 import torch.utils.data as data
 from PIL import Image
+from torchvision import transforms
+import ipdb
+import numpy as np
+import pickle
+st = ipdb.set_trace
 
 def pil_loader(path):
     # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
@@ -115,3 +120,124 @@ class MiniImagenet(data.Dataset):
 
     def __len__(self):
         return len(self._data)
+
+
+
+class Clevr(data.Dataset):
+    def __init__(self, root, mod="temp", train=False, valid=False, test=False,
+                 transform=None, target_transform=None, download=False):
+        super(Clevr, self).__init__()
+        self.root = root
+        self.mod_folder = f"{root}/npys"
+        self.transform = transform
+        self.target_transform = target_transform
+        self.image_folder = os.path.join(os.path.expanduser(root), 'images')
+        self._data = []
+        
+        if train:
+            modfile = f"{self.mod_folder}/{mod}t.txt"
+        elif valid:
+            modfile = f"{self.mod_folder}/{mod}v.txt"
+        elif test:
+            modfile = f"{self.mod_folder}/{mod}v.txt"
+        else:
+            raise ValueError('Unknown split.')
+
+        self._data = []
+        # Extract filenames and labels
+        with open(modfile, 'r') as f:
+            for filename_i in f.readlines():
+                filename = filename_i[:-1]
+                filepath = f"{self.mod_folder}/{filename}"
+                self._data.append(filepath)
+        # self._fit_label_encoding()
+
+    def trees_rearrange(self,tree):
+        tree,boxes,classes = bbox_rearrange(tree,boxes=[],classes=[])
+        # st()
+        boxes = np.stack(boxes)
+        classes = np.stack(classes)
+        N,_  = boxes.shape 
+        scores = np.pad(np.ones([N]),[0,hyp.N-N])
+        boxes = np.pad(boxes,[[0,hyp.N-N],[0,0]])
+        # st()
+        classes = np.pad(classes,[0,hyp.N-N])
+        # boxes.append(boxes[-1])
+        return boxes,scores,classes
+
+    def __getitem__(self, index):
+        data = pickle.load(open(self._data[index],"rb"))
+        view_to_take = np.random.randint(0,24)
+        random_rgb = data['rgb_camXs_raw'][view_to_take][...,:3]
+        tree_file_name = data['tree_seq_filename']
+        tree_file_path = f"{self.root}/tree_file_name"
+        trees = pickle.load(open(tree_file_path,"rb"))
+        gt_boxesR,scores,classes = nlu.trees_rearrange(trees)
+        st()
+
+        # label = self._label_encoder[label]
+        # tree_file = data['tree_seq_filename']
+        # tree_file_path = f""
+        # st()
+        if self.transform is not None:
+            image = self.transform(random_rgb)
+        # if self.target_transform is not None:
+        #     label = self.target_transform(label)
+        label = 0
+        # st()
+        return image, label
+
+    def _fit_label_encoding(self):
+        _, labels = zip(*self._data)
+        unique_labels = set(labels)
+        self._label_encoder = dict((label, idx)
+            for (idx, label) in enumerate(unique_labels))
+
+    def _check_exists(self):
+        return (os.path.exists(self.image_folder) 
+            and os.path.exists(self.split_filename))
+
+    def download(self):
+        from shutil import copyfile
+        from zipfile import ZipFile
+
+        # If the image folder already exists, break
+        if self._check_exists():
+            return True
+
+        # Create folder if it does not exist
+        root = os.path.expanduser(self.root)
+        if not os.path.exists(root):
+            os.makedirs(root)
+
+        # Copy the file to root
+        path_source = os.path.join(self.base_folder, self.filename)
+        path_dest = os.path.join(root, self.filename)
+        print('Copy file `{0}` to `{1}`...'.format(path_source, path_dest))
+        copyfile(path_source, path_dest)
+
+        # Extract the dataset
+        print('Extract files from `{0}`...'.format(path_dest))
+        with ZipFile(path_dest, 'r') as f:
+            f.extractall(root)
+
+        # Copy CSV files
+        for split in self.splits:
+            path_source = os.path.join(self.base_folder, self.splits[split])
+            path_dest = os.path.join(root, self.splits[split])
+            print('Copy file `{0}` to `{1}`...'.format(path_source, path_dest))
+            copyfile(path_source, path_dest)
+        print('Done!')
+
+    def __len__(self):
+        return len(self._data)
+
+if __name__ == "__main__":
+    transform = transforms.Compose([
+        # transforms.RandomResizedCrop(128),        
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])    
+    clevr = Clevr("/media/mihir/dataset/clevr_veggies/",mod="cg",train=True,transform=transform)
+    fixed_images, _ = next(iter(clevr))
+    print("hello")
--- vqvae.py
+++ vqvae.py
@@ -5,15 +5,20 @@ from torchvision import transforms, datasets
 from torchvision.utils import save_image, make_grid
 
 from modules import VectorQuantizedVAE, to_scalar
-from datasets import MiniImagenet
-
+from datasets import MiniImagenet,Clevr
+import ipdb
+st = ipdb.set_trace
 from tensorboardX import SummaryWriter
+import time
 
-def train(data_loader, model, optimizer, args, writer):
+def train(data_loader, model, optimizer, args, writer,epoch):    
+    steps = 0
     for images, _ in data_loader:
+        start_iter_time = time.time()
         images = images.to(args.device)
 
         optimizer.zero_grad()
+
         x_tilde, z_e_x, z_q_x = model(images)
 
         # Reconstruction loss
@@ -26,12 +31,16 @@ def train(data_loader, model, optimizer, args, writer):
         loss = loss_recons + loss_vq + args.beta * loss_commit
         loss.backward()
 
-        # Logs
         writer.add_scalar('loss/train/reconstruction', loss_recons.item(), args.steps)
         writer.add_scalar('loss/train/quantization', loss_vq.item(), args.steps)
 
         optimizer.step()
         args.steps += 1
+        steps += 1
+        iter_time_taken = time.time() - start_iter_time
+        print("(%s) Iters:[%4d/%4d]; Epochs:[%4d/%4d]; Iter Time:%.3f loss: %.3f" % (args.output_folder,steps,len(data_loader),epoch,args.num_epochs,iter_time_taken,loss))
+
+
 
 def test(data_loader, model, args, writer):
     with torch.no_grad():
@@ -86,22 +95,42 @@ def main(args):
                 train=True, download=True, transform=transform)
             test_dataset = datasets.CIFAR10(args.data_folder,
                 train=False, transform=transform)
-            num_channels = 3
+            num_channels = 3    
         valid_dataset = test_dataset
-    elif args.dataset == 'miniimagenet':
+    elif args.dataset == 'clevr':
         transform = transforms.Compose([
-            transforms.RandomResizedCrop(128),
+            # transforms.RandomResizedCrop(128),
             transforms.ToTensor(),
             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
         ])
+        import socket
+        if "Alien" in socket.gethostname():
+            dataset_name = "/media/mihir/dataset/clevr_veggies/"
+        else:
+            dataset_name = "/projects/katefgroup/datasets/clevr_veggies/"
+
         # Define the train, valid & test datasets
-        train_dataset = MiniImagenet(args.data_folder, train=True,
-            download=True, transform=transform)
-        valid_dataset = MiniImagenet(args.data_folder, valid=True,
-            download=True, transform=transform)
-        test_dataset = MiniImagenet(args.data_folder, test=True,
-            download=True, transform=transform)
+        train_dataset = Clevr(dataset_name,mod = args.modname\
+            , train=True, transform=transform)
+        valid_dataset = Clevr(dataset_name,mod = args.modname,\
+         valid=True,transform=transform)
+        test_dataset = Clevr(dataset_name,mod = args.modname,\
+         test=True, transform=transform)
         num_channels = 3
+    # elif args.dataset == 'miniimagenet':
+    #     transform = transforms.Compose([
+    #         transforms.RandomResizedCrop(128),
+    #         transforms.ToTensor(),
+    #         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    #     ])
+    #     # Define the train, valid & test datasets
+    #     train_dataset = MiniImagenet(args.data_folder, train=True,
+    #         download=True, transform=transform)
+    #     # valid_dataset = MiniImagenet(args.data_folder, valid=True,
+    #     #     download=True, transform=transform)
+    #     # test_dataset = MiniImagenet(args.data_folder, test=True,
+    #     #     download=True, transform=transform)
+    #     # num_channels = 3
 
     # Define the data loaders
     train_loader = torch.utils.data.DataLoader(train_dataset,
@@ -112,6 +141,7 @@ def main(args):
         num_workers=args.num_workers, pin_memory=True)
     test_loader = torch.utils.data.DataLoader(test_dataset,
         batch_size=16, shuffle=True)
+    fixed_images, _ = next(iter(train_loader))
 
     # Fixed images for Tensorboard
     fixed_images, _ = next(iter(test_loader))
@@ -128,19 +158,20 @@ def main(args):
 
     best_loss = -1.
     for epoch in range(args.num_epochs):
-        train(train_loader, model, optimizer, args, writer)
+        train(train_loader, model, optimizer, args, writer, epoch)
         loss, _ = test(valid_loader, model, args, writer)
-
-        reconstruction = generate_samples(fixed_images, model, args)
+        reconstructiozn = generate_samples(fixed_images, model, args)
         grid = make_grid(reconstruction.cpu(), nrow=8, range=(-1, 1), normalize=True)
+
         writer.add_image('reconstruction', grid, epoch + 1)
+
 
         if (epoch == 0) or (loss < best_loss):
             best_loss = loss
             with open('{0}/best.pt'.format(save_filename), 'wb') as f:
                 torch.save(model.state_dict(), f)
-        with open('{0}/model_{1}.pt'.format(save_filename, epoch + 1), 'wb') as f:
-            torch.save(model.state_dict(), f)
+        # with open('{0}/model_{1}.pt'.format(save_filename, epoch + 1), 'wb') as f:
+        #     torch.save(model.state_dict(), f)
 
 if __name__ == '__main__':
     import argparse
@@ -150,33 +181,35 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='VQ-VAE')
 
     # General
-    parser.add_argument('--data-folder', type=str,
+    parser.add_argument('--modname', type=str,
         help='name of the data folder')
-    parser.add_argument('--dataset', type=str,
+    parser.add_argument('--dataset', type=str,default="clevr",
         help='name of the dataset (mnist, fashion-mnist, cifar10, miniimagenet)')
 
     # Latent space
     parser.add_argument('--hidden-size', type=int, default=256,
         help='size of the latent vectors (default: 256)')
+
+
     parser.add_argument('--k', type=int, default=512,
         help='number of latent vectors (default: 512)')
 
     # Optimization
-    parser.add_argument('--batch-size', type=int, default=128,
-        help='batch size (default: 128)')
-    parser.add_argument('--num-epochs', type=int, default=100,
-        help='number of epochs (default: 100)')
+    parser.add_argument('--batch-size', type=int, default=32,
+        help='batch size (default: 32)')
+    parser.add_argument('--num-epochs', type=int, default=2000,
+        help='number of epochs (default: 2000)')
     parser.add_argument('--lr', type=float, default=2e-4,
         help='learning rate for Adam optimizer (default: 2e-4)')
     parser.add_argument('--beta', type=float, default=1.0,
         help='contribution of commitment loss, between 0.1 and 2.0 (default: 1.0)')
 
     # Miscellaneous
-    parser.add_argument('--output-folder', type=str, default='vqvae',
+    parser.add_argument('--output-folder', type=str, default='models/vqvae',
         help='name of the output folder (default: vqvae)')
-    parser.add_argument('--num-workers', type=int, default=mp.cpu_count() - 1,
+    parser.add_argument('--num-workers', type=int, default=1,
         help='number of workers for trajectories sampling (default: {0})'.format(mp.cpu_count() - 1))
-    parser.add_argument('--device', type=str, default='cpu',
+    parser.add_argument('--device', type=str, default='cuda',
         help='set the device (cpu or cuda, default: cpu)')
 
     args = parser.parse_args()
@@ -190,8 +223,10 @@ if __name__ == '__main__':
     args.device = torch.device(args.device
         if torch.cuda.is_available() else 'cpu')
     # Slurm
-    if 'SLURM_JOB_ID' in os.environ:
-        args.output_folder += '-{0}'.format(os.environ['SLURM_JOB_ID'])
+    temp_folder = args.output_folder
+    args.output_folder = f"{temp_folder}_K-{args.k}_mod-{args.modname}"
+    # if 'SLURM_JOB_ID' in os.environ:
+    #     args.output_folder += '-{0}'.format(os.environ['SLURM_JOB_ID'])
     if not os.path.exists('./models/{0}'.format(args.output_folder)):
         os.makedirs('./models/{0}'.format(args.output_folder))
     args.steps = 0
